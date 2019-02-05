72b696d6d35b633d497165219d238fe4962df05e
Cook1619 <cook6348@gmail.com>
2019-02-05 17:26:10 -0600

    Passing data mulriple ways through bind

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  //Class components are referred to as containers, because they contain state and typically display the stateless components from within
   state = {
     persons: [
       {name:'Matt', age: 32},
@@ -11,11 +12,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //setState is a method on the react object which we imported at the top of the file
     this.setState({
         persons: [
-          {name:'Mathew', age: 32},
+          {name:newName, age: 32},
           {name:'Danelle', age: 33},
           {name: 'Ahmed', age: 35}
         ]
@@ -26,10 +27,17 @@ class App extends Component {
     return (
       <div className="App">
         <h2>Testing from a react app</h2>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Kids</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Mathew!!!')}>Switch name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this, 'Matt!!!!')}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}>My Hobbies: Kids</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react';
+//It important to use stateless components as much as possible, because stateless fucntions are clear about what they do, and it helps in troubleshooting code. They are dynamic as well by passing props, they don't manipulate application state. 
 
 const person = (props) => {
     return (
         <div>
-            <p>I'm {props.name}, and I am {props.age} years old.</p>
+            <p onClick={props.click}>I'm {props.name}, and I am {props.age} years old.</p>
             <p>{props.children}</p>
         </div>
     )
