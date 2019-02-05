7ed637f6d576bda1f5f5e28ba9102850cc543911
Cook1619 <cook6348@gmail.com>
2019-02-05 09:18:57 -0600

    Adding a functn which upon button click will update the state of the application

--- src/App.js
+++ src/App.js
@@ -7,14 +7,26 @@ class App extends Component {
     persons: [
       {name:'Matt', age: 32},
       {name:'Danelle', age: 33},
-      {name: 'Ahemd', age: 31}
+      {name: 'Ahmed', age: 31}
     ]
   }
+
+  switchNameHandler = () => {
+    //setState is a method on the react object which we imported at the top of the file
+    this.setState({
+        persons: [
+          {name:'Mathew', age: 32},
+          {name:'Danelle', age: 33},
+          {name: 'Ahmed', age: 35}
+        ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h2>Testing from a react app</h2>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Kids</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
