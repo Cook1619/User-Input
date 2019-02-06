4d3359cd0c941baa884eef7867d3795948e5423e
Cook1619 <cook6348@gmail.com>
2019-02-06 10:54:41 -0600

    Adding a delete handler

--- src/App.js
+++ src/App.js
@@ -18,16 +18,12 @@ class App extends Component {
 
   // console.log(personsState, otherState);
 
-  switchNameHandler = newName => {
-    // console.log('Was clicked!');
-    this.setState({
-      persons: [
-        { name: "Mathew", age: 32 },
-        { name: "Danelle", age: 33 },
-        { name: "Jules", age: 4 }
-      ]
-    });
-  };
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
   nameChangeHandler = event => {
     this.setState({
       persons: [
@@ -56,26 +52,17 @@ class App extends Component {
 
     let persons = null;
 
-    if(this.state.showPersons) {
+    if (this.state.showPersons) {
       persons = (
         <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-              click={this.switchNameHandler.bind(this, "Matt!!!!")}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-            >
-              My Hobbies: Kids
-            </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-              changed={this.nameChangeHandler}
-            />
-          </div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+            name={person.name} 
+            age={person.age} 
+            click={() => this.deletePersonHandler(index)}
+            />;
+          })}
+        </div>
       );
     }
     return (
